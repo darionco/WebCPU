9e36c6072f56e5e91f9cd1735321df15fbd3f2ec
Dario Segura <dario.seco@gmail.com>
2019-03-13 23:14:16 -0400

    [CPU] Uses core-estimator https://github.com/oftn-oswg/core-estimator

--- src/WebCPU.worker.js
+++ src/WebCPU.worker.js
@@ -1,17 +1,63 @@
-let data = null;
 let view = null;
-let count = 0;
+let width = 1024;
+let height = 1024;
+
+function checkIfBelongsToMandelbrotSet(x, y, maxIterations = 350) {
+    let realComponentOfResult = x;
+    let imaginaryComponentOfResult = y;
+    // Set max number of iterations
+    for (let i = 0; i < maxIterations; i++) {
+        const tempRealComponent = realComponentOfResult * realComponentOfResult - imaginaryComponentOfResult * imaginaryComponentOfResult + x;
+        const tempImaginaryComponent = 2.0 * realComponentOfResult * imaginaryComponentOfResult + y;
+        realComponentOfResult = tempRealComponent;
+        imaginaryComponentOfResult = tempImaginaryComponent;
+        // Return a number as a percentage
+        if (realComponentOfResult * imaginaryComponentOfResult > 5) {
+            return (i / maxIterations);
+        }
+    }
+    // Return zero if in set
+    return 0;
+}
+
+function runMandelbrot(magnificationFactor = 8500, maxIterations = 350, panX = 0.8, panY = 0.4) {
+    for (let x = 0; x < width; x++) {
+        for (let y = 0; y < height; y++) {
+            const belongsToSet = checkIfBelongsToMandelbrotSet(x / magnificationFactor - panX, y / magnificationFactor - panY, maxIterations);
+            view[0] += y * width + x;
+            view[1] += Math.floor(belongsToSet);
+        }
+    }
+}
+
+function runWorkload(workload) {
+    let a = 0x08a90db3;
+    let b = 0xabd209a0;
+    let c = 0x29019b32;
+    let d = 0x01ab3291;
+
+    for (let i = 0; i < workload; i++) {
+        a = (b ^ a) >> 1;
+        b = (c ^ b) << 1;
+        c = (d ^ c) >> 1;
+        d = (a ^ d) << 1;
+    }
+
+    return a;
+}
 
 self.onmessage = e => {
     const message = e.data;
 
     switch (message.type) {
         case 'init':
-            if (!data) {
-                data = message.data;
-                view = new Float32Array(data);
-                count = view.length * 0.5;
-
+            if (!view) {
+                width = message.width ? message.width : width;
+                height = message.height ? message.height : height;
+                view = new Uint32Array(2);
+                // for (let i = 0; i < 10; ++i) {
+                //     runMandelbrot();
+                // }
                 self.postMessage('success');
             } else {
                 self.postMessage('error');
@@ -19,60 +65,17 @@ self.onmessage = e => {
             break;
 
         case 'compute': {
-            let dot = 0.0;
-            let lengthA = 0.0;
-            let lengthB = 0.0;
+            setTimeout(() => {
+                const start = performance.now();
+                runMandelbrot(message.params.magnificationFactor, message.params.maxIterations, message.params.panX, message.params.panY);
+                const result = performance.now() - start;
+                self.postMessage(result);
+            }, message.startTime - Date.now());
+            break;
+        }
 
-            for (let i = 0; i < count; ++i) {
-                lengthA += Math.sqrt(view[i] * view [i]);
-                lengthB += Math.sqrt(view[i + count] * view[i + count]);
-                dot += view[i] * view[i + count];
-            }
-
-            for (let i = 0; i < count; ++i) {
-                lengthA += Math.atan2(view[i], view[i + count]);
-                lengthB += Math.atan2(view[i + count], view[i]);
-                dot += Math.atan(view[i] * view[i + count]);
-            }
-
-            for (let i = count - 1; i >= 0; --i) {
-                lengthA -= Math.acos(view[i] * view[i]);
-                lengthB -= Math.acos(view[i + count] * view[i + count]);
-                dot -= Math.acos(view[i] * view[i + count]);
-            }
-
-            for (let i = count - 1; i >= 0; --i) {
-                lengthA -= Math.log2(view[i] * view[i]);
-                lengthB -= Math.log2(view[i + count] * view[i + count]);
-                dot -= Math.log2(view[i] * view[i + count]);
-            }
-
-            for (let i = 0; i < count; ++i) {
-                lengthA += Math.sin(view[count - i - 1] * view[count - i - 1]);
-                lengthB += Math.sin(view[count * 2 - i - 1] * view[count * 2 - i - 1]);
-                dot += Math.sin(view[count - i - 1] * view[count * 2 - i - 1]);
-            }
-
-            for (let i = 0; i < count; ++i) {
-                lengthA += Math.asin(view[count - i - 1] * view[count - i - 1]);
-                lengthB += Math.asin(view[count * 2 - i - 1] * view[count * 2 - i - 1]);
-                dot += Math.asin(view[count - i - 1] * view[count * 2 - i - 1]);
-            }
-
-            for (let i = 0; i < count; ++i) {
-                lengthA -= Math.log10(view[count - i - 1] * view[count - i - 1]);
-                lengthB -= Math.log10(view[count * 2 - i - 1] * view[count * 2 - i - 1]);
-                dot -= Math.log10(view[count - i - 1] * view[count * 2 - i - 1]);
-            }
-
-            for (let i = 0; i < count; ++i) {
-                lengthA -= Math.tan(view[count - i - 1] * view[count - i - 1]);
-                lengthB -= Math.tan(view[count * 2 - i - 1] * view[count * 2 - i - 1]);
-                dot -= Math.tan(view[count - i - 1] * view[count * 2 - i - 1]);
-            }
-
-            const result = Math.sqrt(dot) / (lengthA * lengthB);
-            self.postMessage(result);
+        case 'workload': {
+            self.postMessage(runWorkload(message.workload));
             break;
         }
 
--- src/index.js
+++ src/index.js
@@ -1,74 +1,65 @@
 import CPUWorker from 'worker-loader?inline=true!./WebCPU.worker';
+import {getHardwareConcurrency} from './CoreEstimator';
 
 export class WebCPU {
-    static async detectCPU(hardcore = true) {
-        const data = this._generateData(1024 * 8);
-        const workers = [];
-        const loops = 100;
-        let baseTime = 0;
-        let start;
-        let end;
-
-        workers.push(await this._initWorker(data.slice(0)));
-
-        for (let n = 0; n < loops; ++n) {
-            start = performance.now();
-            await this._computeWorker(workers[0]);
-            end = performance.now();
-            baseTime += end - start;
-        }
-        baseTime /= loops;
-        console.log(`Base Time: ${baseTime}`);
-        this._killWorkers(workers);
-
-        const maxThresholdCount = 3;
-        const threshold = 1.05;
-        let thresholdCount = 0;
-        let threadCount = 0;
-        let thresholdThreads = 0;
-        while (true) {
-            ++threadCount;
-            const promises = [];
-            for (let i = workers.length; i < threadCount; ++i) {
-                promises.push(this._initWorker(data.slice(0)).then(worker => workers.push(worker)));
-            }
-            await Promise.all(promises);
-            promises.length = 0;
-
-            let time = 0;
-            for (let n = 0; n < loops; ++n) {
-                start = performance.now();
-                for (let i = 0; i < workers.length; ++i) {
-                    promises.push(this._computeWorker(workers[i]));
-                }
-                await Promise.all(promises);
-                end = performance.now();
-                time += end - start;
-            }
-            time /= loops;
-            console.log(`Threads:${workers.length} Took:${time}ms Diff:${time / baseTime}`);
-            if (time / baseTime > threshold) {
-                if (!thresholdThreads) {
-                    thresholdThreads = threadCount - 1;
-                }
-
-                if (++thresholdCount >= maxThresholdCount) {
-                    break;
-                }
-            } else {
-                thresholdCount = 0;
-                thresholdThreads = 0;
-            }
-
-            if (hardcore) {
-                this._killWorkers(workers);
-            }
-        }
-
-        return {
-            physicalCores: thresholdThreads,
-            logicalCores: navigator.hardwareConcurrency ? navigator.hardwareConcurrency : thresholdThreads,
-        };
+    static async detectCPU() {
+        return await getHardwareConcurrency();
+        // const workers = [];
+        // const times = [];
+        // const loops = 20;
+        // let size = 128;
+        // let baseTime;
+        //
+        // workers.push(await this._initWorker(size, size));
+        // baseTime = await this._testWorkers(workers, loops);
+        // times.push(baseTime);
+        // console.log(`Base Time: ${baseTime}`);
+        //
+        // if (hardcore) {
+        //     this._killWorkers(workers);
+        // }
+        //
+        // const maxThresholdCount = 3;
+        // const threshold = 1.15;
+        // let thresholdCount = 0;
+        // let threadCount = 0;
+        // let thresholdThreads = 0;
+        // while (true) {
+        //     ++threadCount;
+        //     const promises = [];
+        //     for (let i = workers.length; i < threadCount; ++i) {
+        //         promises.push(this._initWorker(size, size).then(worker => workers.push(worker)));
+        //     }
+        //     await Promise.all(promises);
+        //     promises.length = 0;
+        //
+        //     const time = await this._testWorkers(workers, loops);
+        //     const expected = this._getExpectedNextOffset(times, baseTime);
+        //     const diff = time - times[times.length - 1];
+        //     times.push(time);
+        //     console.log(`Threads:${workers.length} Took:${time}ms Expected:${expected} Diff:${diff}`);
+        //     if (time / baseTime > threshold) {
+        //         if (!thresholdThreads) {
+        //             thresholdThreads = threadCount - 1;
+        //         }
+        //
+        //         if (++thresholdCount >= maxThresholdCount) {
+        //             break;
+        //         }
+        //     } else {
+        //         thresholdCount = 0;
+        //         thresholdThreads = 0;
+        //     }
+        //
+        //     if (hardcore) {
+        //         this._killWorkers(workers);
+        //     }
+        // }
+        //
+        // return {
+        //     physicalCores: thresholdThreads,
+        //     logicalCores: navigator.hardwareConcurrency ? navigator.hardwareConcurrency : thresholdThreads,
+        // };
     }
 
     static _killWorkers(workers) {
@@ -77,17 +68,53 @@ export class WebCPU {
         }
     }
 
-    static _computeWorker(worker) {
+    static _getExpectedNextOffset(times, baseTime) {
+        let total = times[0] - baseTime;
+        for (let i = 1; i < times.length; ++i) {
+            total += times[i] - times[i - 1];
+        }
+        return total / times.length;
+    }
+
+    static async _testWorkers(workers, loops) {
+        const params = this._generateParams();
+        const promises = [];
+        const extraLoops = 10;
+        const startTime = Date.now() + workers.length * 10;
+        let results;
+        let time = 0;
+        for (let n = 0; n < loops + extraLoops; ++n) {
+            params.panX += 0.0001;
+            params.panY += 0.0001;
+            for (let i = 0; i < workers.length; ++i) {
+                promises.push(this._computeWorker(workers[i], params, startTime));
+            }
+            results = await Promise.all(promises);
+            let rs = '';
+            if (n >= extraLoops) {
+                for (let i = 0; i < results.length; ++i) {
+                    rs += `T${i}:${results[i]} `;
+                    time += results[i];
+                }
+            }
+            // console.log(rs);
+            promises.length = 0;
+        }
+        time /= loops * workers.length;
+        return time;
+    }
+
+    static _computeWorker(worker, params, startTime) {
         return new Promise(resolve => {
             worker.onmessage = e => {
                 worker.onmessage = null;
                 resolve(e.data);
             };
-            worker.postMessage({ type: 'compute' });
+            worker.postMessage({ type: 'workload', params, startTime, workload: 0x400000 });
         });
     }
 
-    static _initWorker(data) {
+    static _initWorker(width, height) {
         return new Promise((resolve, reject) => {
             const worker = new CPUWorker();
 
@@ -101,19 +128,16 @@ export class WebCPU {
                 }
             };
 
-            worker.postMessage({ type: 'init', data }, data);
+            worker.postMessage({ type: 'init', width, height });
         });
     }
 
-    static _generateData(count) {
-        const buffer = new ArrayBuffer(count * 2 * 4);
-        const view = new Float32Array(buffer);
-
-        for (let i = 0; i < count; ++i) {
-            view[i] = Math.random() + 0.00001;
-            view[i + count] = Math.random() + 0.00001;
+    static _generateParams() {
+        return {
+            magnificationFactor: 3800,
+            maxIterations: 350,
+            panX: 0.2,
+            panY: 0.2,
         }
-
-        return buffer;
     }
 }
