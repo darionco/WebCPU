d38b33b5d1200a37e2b6647c802a65430fce7507
Dario Segura <dario.seco@gmail.com>
2019-03-15 00:16:57 -0400

    [WORKER] Removed Mandelbrot workload

--- src/WebCPU.worker.js
+++ src/WebCPU.worker.js
@@ -1,59 +1,3 @@
-function checkIfBelongsToMandelbrotSet(x, y, maxIterations = 350) {
-    let realComponentOfResult = x;
-    let imaginaryComponentOfResult = y;
-    // Set max number of iterations
-    for (let i = 0; i < maxIterations; i++) {
-        const tempRealComponent = realComponentOfResult * realComponentOfResult - imaginaryComponentOfResult * imaginaryComponentOfResult + x;
-        const tempImaginaryComponent = 2.0 * realComponentOfResult * imaginaryComponentOfResult + y;
-        realComponentOfResult = tempRealComponent;
-        imaginaryComponentOfResult = tempImaginaryComponent;
-        // Return a number as a percentage
-        if (realComponentOfResult * imaginaryComponentOfResult > 5) {
-            return (i / maxIterations);
-        }
-    }
-    // Return zero if in set
-    return 0;
-}
-
-function runMandelbrot(duration, id) {
-    const width = 32784;
-    const height = 32784;
-    let magnificationFactor = 3800;
-    let maxIterations = 350;
-    let panX = 0.2;
-    let panY = 0.2;
-
-    let coordinate = 0;
-    let pixel = 0;
-
-    const start = performance.now();
-    let end = start;
-
-    let i = 0;
-    while (end - start < duration) {
-        panX += 0.01;
-        panY += 0.01;
-        magnificationFactor += 1;
-        for (let x = 0; x < width && end - start < duration; x++) {
-            for (let y = 0; y < height && end - start < duration; y++) {
-                const belongsToSet = checkIfBelongsToMandelbrotSet(x / magnificationFactor - panX, y / magnificationFactor - panY, maxIterations);
-                coordinate |= y * width + x;
-                pixel |= Math.floor(belongsToSet);
-                end = performance.now();
-                ++i;
-            }
-        }
-    }
-
-    return {
-        elapsed: end - start,
-        iterations: i,
-        result: coordinate ^ pixel,
-        id,
-    };
-}
-
 function runWorkload(duration, id) {
     const start = performance.now();
     let end = start;
@@ -83,17 +27,9 @@ self.onmessage = e => {
 
     switch (message.type) {
         case 'init':
-            runWorkload(2, 0);
-            runMandelbrot(2, 0);
+            runWorkload(3, 0);
             self.postMessage('success');
             break;
-
-        case 'mandelbrot': {
-            setTimeout(() => {
-                self.postMessage(runMandelbrot(10, message.id));
-            }, message.startTime - Date.now());
-            break;
-        }
 
         case 'workload': {
             setTimeout(() => {
