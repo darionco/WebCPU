1a912c6f74654e1c6e4eb048a78566577b8c34a8
Dario Segura <darionco.dev@gmail.com>
2019-04-10 23:56:19 -0400

    [ESLINT] Pass

--- src/WebCPU.worker.js
+++ src/WebCPU.worker.js
@@ -36,16 +36,13 @@ function runWorkloadJS(duration, id) {
     let end = start;
 
     for (ii = 0; end - start < duration; ++ii, end = performance.now()) {
-        for(y = 0; y < 200; ++y)
-        {
-            for(x = 0; x < 200; ++x)
-            {
+        for (y = 0; y < 200; ++y) {
+            for (x = 0; x < 200; ++x) {
                 cx = -2 + x / 50;
                 cy = -2 + y / 50;
                 i = 0;
 
-                do
-                {
+                do {
                     xt = cx * cx - cy * cy + creal;
                     cy = 2 * cx * cy + cimag;
                     cx = xt;
@@ -53,9 +50,9 @@ function runWorkloadJS(duration, id) {
                 while ((cx * cx + cy * cy < 4) && ++i < 25);
             }
         }
-        ++frame;        // increase the number of the frame
-        creal= -0.8 + 0.6 * sin(frame / (M_PI * 20));    // calculate the new coordinates
-        cimag= 0.156 + 0.4 * cos(frame / (M_PI * 40));   // of the c point
+        ++frame; // increase the number of the frame
+        creal = -0.8 + 0.6 * sin(frame / (M_PI * 20)); // calculate the new coordinates
+        cimag = 0.156 + 0.4 * cos(frame / (M_PI * 40)); // of the c point
     }
 
     return {
@@ -110,7 +107,6 @@ self.onmessage = e => {
                 runWorkload = runWorkloadWASM;
             } else {
                 runWorkload = runWorkloadJS;
-
             }
             runWorkload(1, 0);
             self.postMessage('success');
